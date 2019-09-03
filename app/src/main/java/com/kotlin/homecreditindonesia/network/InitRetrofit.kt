4abf42c54a0ae04ead0be95b8490fca65f2fc5d6
Mories <moriesdeo242@gmail.com>
2019-09-03 17:29:14 +0700

    First Commit

--- app/src/main/java/com/kotlin/homecreditindonesia/network/InitRetrofit.kt
+++ app/src/main/java/com/kotlin/homecreditindonesia/network/InitRetrofit.kt
@@ -1,13 +1,13 @@
 package com.kotlin.homecreditindonesia.network
 
+import com.kotlin.homecreditindonesia.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class InitRetrofit {
-    private val BASE_URL = "https://private-a8e48-hcidtest.apiary-mock.com"
 
     private fun init(): Retrofit =
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
 
     fun instance(): RestApi = init().create(RestApi::class.java)
 }
