d9402e1047767a006aa9f31d463888f80e504948
Mories <moriesdeo242@gmail.com>
2019-09-04 22:25:59 +0700

    First Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.50'
-    ext.anko_version = '0.10.5'
+    extra.apply {
+        set("kotlin_version", "1.3.50")
+        set("anko_version", "0.10.5")
+    }
     repositories {
         google()
         jcenter()
-        
+
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -20,10 +22,11 @@ allprojects {
     repositories {
         google()
         jcenter()
-        
+
     }
 }
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
